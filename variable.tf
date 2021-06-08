2619bbf38a7a9f7ae3d5ed77b855a62216ef0567
SullyCLT <82404012+SullyCLT@users.noreply.github.com>
2021-06-08 15:47:52 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,7 @@
-
+# Main variables:
+# region: (String) the reg to deploy to
+# app_name: (String) the name of the app to deploy
+# shard_count: (Number) the # of shards to use for the stream
+# retention_period: (Number) length of time data reords remain accessible after being added to the stream
+# s3_bucket_arn: (String) s3 bucket arn for kinesis firehose to dump data
+# s3_bucket_path: (String) s3 bucket path for kinesis firehose to dump data
